63619bf96c2d29b7b2da4d9c57aae863cf173bd1
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-27 22:01:14 -0400

    update

--- main.c
+++ main.c
@@ -7,3 +7,9 @@ int main(void) {
 int choice, i, a, b;
 float x, y, result;
 clrscr();
+
+do {
+printf(“\nSelect your operation (0 to exit):\n”);
+printf(“1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n”);
+printf(“5. Square root\n6. X ^ Y\n7. X ^ 2\n8. X ^ 3\n”);
+printf(“9. 1 / X\n10. X ^ (1 / Y)\n11. X ^ (1 / 3)\n”);
