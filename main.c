42e0a1e1b370b0154ab69a369c7e219c9d61c04e
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-27 22:01:39 -0400

    update

--- main.c
+++ main.c
@@ -13,3 +13,7 @@ printf(“\nSelect your operation (0 to exit):\n”);
 printf(“1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n”);
 printf(“5. Square root\n6. X ^ Y\n7. X ^ 2\n8. X ^ 3\n”);
 printf(“9. 1 / X\n10. X ^ (1 / Y)\n11. X ^ (1 / 3)\n”);
+printf(“12. 10 ^ X\n13. X!\n14. %\n15. log10(x)\n16. Modulus\n”);
+printf(“17. Sin(X)\n18. Cos(X)\n19. Tan(X)\n20. Cosec(X)\n”);
+printf(“21. Cot(X)\n22. Sec(X)\n”);
+printf(“Choice: “);
