0ba2378a217924541314a30967ec53ee0915c90b
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-03-27 22:04:48 -0400

    update

--- main.c
+++ main.c
@@ -39,3 +39,12 @@ do {
     printf(“\nResult: %f”, result);
     break;
 
+    case 3:
+    printf(“Enter X: “);
+    scanf(“%f”, &x);
+    printf(“\nEnter Y: “);
+    scanf(“%f”, &y);
+    result = x * y;
+    printf(“\nResult: %f”, result);
+    break;
+
